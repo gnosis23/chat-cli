55dd403f9f7e607477587837f81b200420b1c95e
BOHAO WANG <bj050323@gmail.com>
2025-07-30 21:29:08 +0800

    feat: commands

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,7 +1,18 @@
 import { configCommand } from './config-command.js';
 import { toolsCommand } from './tools-command.js';
 
+function showCommand() {
+	const keys = Object.keys(commands);
+	console.log('--------------------------------------');
+	console.log('Commands:');
+	keys.forEach((key) => {
+		console.log(`- ${key}`);
+	});
+	console.log('--------------------------------------\n');
+}
+
 export const commands = {
 	'/config': configCommand,
 	'/tools': toolsCommand,
+	'/commands': showCommand,
 };
